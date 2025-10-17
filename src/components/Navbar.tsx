437372b296078bd571670fb05b5fa6a7f6d2b671
Lin <tzu.hsun.lin046@gmail.com>
2025-10-17 10:22:52 +0800

    Navbar樣式調整

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar:FC = () =>{
                 <nav className='max-w-[92%] mx-auto flex items-center justify-between'>
                     <img src={logo} alt="" className='w-auto h-[40px] object-cover '/>
                     <div className="max-w-[34%] w-full flex items-center justify-between">
-                        <div className="w-full flex items-center justify-between">
+                        <div className="max-w-[70%] w-full flex items-center justify-between">
                             <span className='text-primary font-semibold'>首頁</span>
                             <span className='text-primary font-semibold'>甜點</span>
                             <span className='text-primary font-semibold'>登入</span>
