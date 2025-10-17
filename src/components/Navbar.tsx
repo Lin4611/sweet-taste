583ccdad1038634dd8d0e548d2ba008b188d1015
Lin <tzu.hsun.lin046@gmail.com>
2025-10-17 10:43:52 +0800

    將Navbar加入rwd樣式

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
 import type { FC } from 'react'
 import logo from '../assets/img/logo/logo-all-dark.svg'
 import cart from '../assets/img/icon/shopping_cart.png'
+import logo_phone from '../assets/img/logo/logotype-sm-dark.svg'
+import menu_icon from '../assets/img/icon/dehaze-24px.png'
 const Navbar:FC = () =>{
     return (
-        <header className='w-full mx-auto py-[30px] sticky top-0 z-[100] bg-white'>
-                <nav className='max-w-[92%] mx-auto flex items-center justify-between'>
-                    <img src={logo} alt="" className='w-auto h-[40px] object-cover '/>
-                    <div className="max-w-[34%] w-full flex items-center justify-between">
-                        <div className="max-w-[70%] w-full flex items-center justify-between">
+        <header className='w-full mx-auto sticky top-0 z-[100] bg-white py-[30px] lg:py-[30px]'>
+                <nav className='mx-auto flex items-center justify-between w-full px-[33px] lg:max-w-[92%]'>
+                    <img src={menu_icon} alt="menu" className='blcok lg:hidden' />
+                    <img src={logo} alt="logo" 
+                    className='hidden lg:block h-[40px] w-auto object-cover'/>
+                    <img src={logo_phone} alt="logo"
+                    className='block h-[18px] w-auto object-cover lg:hidden' />
+                    <div className="items-center justify-between
+                                    lg:flex lg:max-w-[34%]">
+                        <div className="hidden lg:max-w-[70%] lg:flex w-full items-center justify-between">
                             <span className='text-primary font-semibold'>首頁</span>
                             <span className='text-primary font-semibold'>甜點</span>
                             <span className='text-primary font-semibold'>登入</span>
