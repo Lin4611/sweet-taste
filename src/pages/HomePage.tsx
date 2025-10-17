6573e0f73168690681386ba2a8ece39192b388c3
Lin <tzu.hsun.lin046@gmail.com>
2025-10-17 14:26:45 +0800

    refactor: 重構 HomePage 結構，改用 CategoryCard 元件生成分類卡片區塊

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,37 @@
 import type { FC } from "react"
+import CategoryCard from "../components/CategoryCard"
 import banner from '../assets/img/home-banner.avif'
 import today from '../assets/img/today.avif'
-// import popular from '../assets/img/popular.avif'
-// import new_product_pic from '../assets/img/new.avif'
+import popular from '../assets/img/popular.avif'
+import new_product_pic from '../assets/img/new.avif'
 const HomePage:FC = ()=>{
     return(
         <>
             <main className="w-full mx-auto lg:px-[42px]">
                 <img src={banner} alt="" className="w-full h-auto object-cover lg:h-[496px]" />
                 <section className="w-full flex">
-                    <section className="w-[125px] h-auto overflow-hidden relative ">
-                        <img src={today} alt="" className="w-full h-full object-cover" />
-                        <div className="inset-0 absolute bg-accent/80 backdrop-blur-[5px]"></div>
-                        <div className="absolute inset-0 flex items-center justify-center">
-                            <p className="font-semibold text-subtitle text-primary vertical-text">本日精選</p>
-                        </div>
-                    </section>
+                    <CategoryCard 
+                    imgUrl={today}
+                    title='本日精選'
+                    bgColor="bg-accent/80"
+                    textColor="text-primary"
+                    />
+                    <CategoryCard 
+                    imgUrl={popular}
+                    title='人氣推薦'
+                    bgColor="bg-primary/80"
+                    textColor="text-invert"
+                    textWeight="font-light"
+                    border="border-l-[1px] border-r-[1px] border-white"
+                    />
+                    <CategoryCard 
+                    imgUrl={new_product_pic}
+                    title='新品上市'
+                    bgColor="bg-primary/80"
+                    textColor="text-invert" 
+                    textWeight="font-light"
+                    />
+                    
                 </section>
             </main>
         </>
