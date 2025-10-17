b66c9801e2fe818976b8b54bff64c69c87b03ebe
Lin <tzu.hsun.lin046@gmail.com>
2025-10-17 11:10:24 +0800

    chore:增加banner部分

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import type { FC } from "react"
-
+import banner from '../assets/img/home-banner.avif'
 const HomePage:FC = ()=>{
     return(
         <>
-            
+            <main className="w-full">
+                <img src={banner} alt="" className="w-full h-auto object-cover" />
+            </main>
         </>
     )
 }
