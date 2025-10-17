1c0659942c1cc5791d8783ed89ebe05f6a11fdc3
Lin <tzu.hsun.lin046@gmail.com>
2025-10-17 09:43:33 +0800

    建立Navbar 元件

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import logo from '../assets/img/logo/logo-all-dark.svg'
+const Navbar = () =>{
+    return (
+        <header className='w-full mx-auto py-[30px] sticky top-0 z-[100] bg-white'>
+                <nav className='max-w-[92%] mx-auto flex items-center'>
+                    <img src={logo} alt="" className='h-[40px] object-cover w-auto'/>
+                    <div className="max-w-[34%] flex items-center justify-between">
+                        <div className="w-full flex items-center justify-between">
+                            <span>首頁</span>
+                            <span>甜點</span>
+                            <span>登入</span>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+    )
+}
+export default Navbar
