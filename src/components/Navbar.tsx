7f11da71ade78d9b75b7c955947a8f9ccf2a14a5
Lin <tzu.hsun.lin046@gmail.com>
2025-10-17 10:16:56 +0800

    調整樣式設定

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,18 @@
 import type { FC } from 'react'
 import logo from '../assets/img/logo/logo-all-dark.svg'
+import cart from '../assets/img/icon/shopping_cart.png'
 const Navbar:FC = () =>{
     return (
         <header className='w-full mx-auto py-[30px] sticky top-0 z-[100] bg-white'>
-                <nav className='max-w-[92%] mx-auto flex items-center'>
-                    <img src={logo} alt="" className='h-[40px] object-cover w-auto'/>
-                    <div className="max-w-[34%] flex items-center justify-between">
+                <nav className='max-w-[92%] mx-auto flex items-center justify-between'>
+                    <img src={logo} alt="" className='w-auto h-[40px] object-cover '/>
+                    <div className="max-w-[34%] w-full flex items-center justify-between">
                         <div className="w-full flex items-center justify-between">
-                            <span>首頁</span>
-                            <span>甜點</span>
-                            <span>登入</span>
+                            <span className='text-primary font-semibold'>首頁</span>
+                            <span className='text-primary font-semibold'>甜點</span>
+                            <span className='text-primary font-semibold'>登入</span>
                         </div>
+                        <img src={cart} alt="" className='h-[24px] w-auto object-cover aspect-square' />
                     </div>
                 </nav>
             </header>
