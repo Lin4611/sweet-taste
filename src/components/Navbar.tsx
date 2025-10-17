915b6a4d0dfa2903e445789f277a5bde4371ad88
Lin <tzu.hsun.lin046@gmail.com>
2025-10-17 13:44:01 +0800

    style: 調整 Navbar 版面與樣式配置

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import logo_phone from '../assets/img/logo/logotype-sm-dark.svg'
 import menu_icon from '../assets/img/icon/dehaze-24px.png'
 const Navbar:FC = () =>{
     return (
-        <header className='w-full mx-auto sticky top-0 z-[100] bg-white py-[30px] lg:py-[30px]'>
+        <header className='w-full mx-auto sticky top-0 z-[100] bg-white py-[30px] lg:py-[30px] lg:sticky'>
                 <nav className='mx-auto flex items-center justify-between w-full px-[33px] lg:max-w-[92%]'>
                     <img src={menu_icon} alt="menu" className='blcok lg:hidden' />
                     <img src={logo} alt="logo" 
@@ -13,7 +13,7 @@ const Navbar:FC = () =>{
                     <img src={logo_phone} alt="logo"
                     className='block h-[18px] w-auto object-cover lg:hidden' />
                     <div className="items-center justify-between
-                                    lg:flex lg:max-w-[34%]">
+                                    lg:flex lg:max-w-[34%] lg:w-full">
                         <div className="hidden lg:max-w-[70%] lg:flex w-full items-center justify-between">
                             <span className='text-primary font-semibold'>首頁</span>
                             <span className='text-primary font-semibold'>甜點</span>
