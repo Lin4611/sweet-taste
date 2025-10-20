e50cfd8e43e8f93e0ad45100fd35a8f7ba7876cf
Lin <tzu.hsun.lin046@gmail.com>
2025-10-20 14:13:55 +0800

    fix: correct props passing and update hover styles

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -3,28 +3,22 @@ import React from "react";
 interface CategoryCardProps {
   imgUrl: string;
   title: string;
-  bgColor?: string;
-  textColor?: string;
-  textWeight?: string;
   border?: string;
 }
 const CategoryCard: React.FC<CategoryCardProps> = ({
   imgUrl,
   title,
-  bgColor,
-  textColor,
-  textWeight = "font-semibold",
   border,
 }) => {
   return (
     <section
-      className={`max-h-[180px] overflow-hidden relative ${border} lg:max-h-[256px]`}
+      className={`group max-h-[180px] overflow-hidden relative ${border} lg:max-h-[256px]`}
     >
       <img src={imgUrl} alt="" className="w-full h-full object-cover" />
-      <div className={`absolute inset-0 ${bgColor} backdrop-blur-[5px]`}></div>
+      <div className='absolute inset-0 bg-primary/80 backdrop-blur-[5px] group-hover:bg-accent/80'></div>
       <div className="absolute inset-0 flex items-center justify-center">
         <p
-          className={`${textWeight} text-subtitle ${textColor} vertical-text leading-24 tracking-[0.24rem] lg:tracking-[0.3rem]`}
+          className='font-light text-subtitle text-invert vertical-text leading-24 tracking-[0.24rem] group-hover:text-primary lg:tracking-[0.3rem]'
         >
           {title}
         </p>
