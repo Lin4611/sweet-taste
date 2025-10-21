624f18b8c5828370de58224c10057e40bcde2168
Lin <tzu.hsun.lin046@gmail.com>
2025-10-21 15:17:27 +0800

    feat: complete responsive category sidebar section on Product List page

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -7,7 +7,18 @@ const ProductListPage: FC = () => {
         <figure className="w-full  h-auto lg:px-[42px] lg:h-auto">
             <img src={banner_pic} alt="banner" className="w-full h-[277px] object-cover lg:h-auto" />
         </figure>
-        
+        <section className="flex flex-col w-full lg:px-[42px] lg:py-15">
+            <section className="flex justify-between">
+                <ul className="flex flex-col items-center w-full text-primary text-title font-semibold leading-10 text-center lg:w-[300px]">
+                    <p className="bg-primary text-invert py-3 w-full">甜點類別</p>
+                    <li className="py-3 bg-white border border-soft w-full hover:bg-soft/100 active:bg-soft/100">所有甜點（48）</li>
+                    <li className="py-3 bg-white border border-soft w-full hover:bg-soft/100 active:bg-soft/100">本日精選（10）</li>
+                    <li className="py-3 bg-white border border-soft w-full hover:bg-soft/100 active:bg-soft/100">人氣推薦（26）</li>
+                    <li className="py-3 bg-white border border-soft w-full hover:bg-soft/100 active:bg-soft/100">新品上市（12）</li>
+                </ul>
+            </section>
+        </section>
+
       </main>
     </>
   );
