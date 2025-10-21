ea2ebef559c4f1506f99323269764569a86c7e55
Lin <tzu.hsun.lin046@gmail.com>
2025-10-21 10:40:23 +0800

    style: complete desktop layout for ProductList section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,6 +12,7 @@ import why_eat_dessert_pic from "../assets/img/p-7.avif";
 import why_eat_dessert_title from "../assets/img/title-pic/mobile-title-2.svg";
 import why_eat_dessert_title_desk from "../assets/img/title-pic/desktop-title-2.png";
 import want_to_eat_no_reason_title from "../assets/img/title-pic/mobile-title-3.svg";
+import want_to_eat_no_reason_title_desktop from "../assets/img/title-pic/desktop-title-3.svg";
 import product_1 from "../assets/img/p-1.png";
 import product_2 from "../assets/img/p-2.png";
 import product_3 from "../assets/img/p-3.png";
@@ -63,7 +64,7 @@ const HomePage: FC = () => {
   return (
     <>
       <main className="w-full mx-auto max-w-[1024px]">
-        <section className="w-full mx-auto lg:px-[42px] lg:mb-20">
+        <section className="w-full mx-auto lg:px-[42px] lg:mb-5">
           <img
             src={banner}
             alt="banner"
@@ -79,141 +80,158 @@ const HomePage: FC = () => {
             <CategoryCard imgUrl={new_product_pic} title="新品上市" />
           </section>
         </section>
-        <section className="hidden lg:flex lg:flex-col lg:gap-[105px] lg:w-full">
-          <section className="hidden lg:block lg:relative ">
-            <section className="vertical-text flex flex-col w-full h-[420px] bg-soft/100 justify-end items-center ">
-              <section className="max-h-[280px] text-muted overflow-hidden font-light flex flex-col whitespace-nowrap gap-5 ml-[557px]">
-                <p>青山依舊在，幾度夕陽紅。慣看秋月春</p>
-                <p>一壺濁酒喜相逢，浪花淘盡英雄。是非</p>
-                <p>轉頭空，滾滾長江東逝水，白髮漁樵江</p>
-                <p>古今多少事，都付笑談中。</p>
-                <p className="w-4" />
-                <p>是非成敗轉頭空，滾滾長江東逝水，白。</p>
-                <p>樵江渚上，古今多少事，都付笑談中。</p>
+        <section className="lg:py-15">
+          <section className="hidden lg:flex lg:flex-col lg:gap-[105px] lg:w-full lg:mb-20">
+            <section className="hidden lg:block lg:relative ">
+              <section className="vertical-text flex flex-col w-full h-[420px] bg-soft/100 justify-end items-center ">
+                <section className="max-h-[280px] text-muted overflow-hidden font-light flex flex-col whitespace-nowrap gap-5 ml-[557px]">
+                  <p>青山依舊在，幾度夕陽紅。慣看秋月春</p>
+                  <p>一壺濁酒喜相逢，浪花淘盡英雄。是非</p>
+                  <p>轉頭空，滾滾長江東逝水，白髮漁樵江</p>
+                  <p>古今多少事，都付笑談中。</p>
+                  <p className="w-4" />
+                  <p>是非成敗轉頭空，滾滾長江東逝水，白。</p>
+                  <p>樵江渚上，古今多少事，都付笑談中。</p>
+                </section>
               </section>
+              <div className="flex justify-between z-100 -mt-110 mx-[42px]">
+                <img
+                  src={why_make_dessert_pic}
+                  alt=""
+                  className="h-[460px] w-auto aspect-square object-cover "
+                />
+                <img
+                  src={why_make_dessert_title_desk}
+                  alt=""
+                  className="w-[88px] h-[307px] object-cover"
+                />
+              </div>
             </section>
-            <div className="flex justify-between z-100 -mt-110 mx-[42px]">
+            <section className="hidden lg:block lg:relative ">
+              <section className="vertical-text flex flex-col w-full h-[420px] bg-soft/100 justify-end items-center">
+                <section className="max-h-[280px] text-muted overflow-hidden font-light flex flex-col whitespace-nowrap ml-[55.5px] gap-6 pr-10">
+                  <p>青山依舊在，幾度夕陽紅。慣看秋月春</p>
+                  <p>一壺濁酒喜相逢，浪花淘盡英雄。是非</p>
+                  <p>轉頭空，滾滾長江東逝水，白髮漁樵江</p>
+                  <p>古今多少事，都付笑談中。</p>
+                  <p className="w-4"></p>
+                  <p>是非成敗轉頭空，滾滾長江東逝水，白。</p>
+                  <p>樵江渚上，古今多少事，都付笑談中。</p>
+                </section>
+              </section>
+              <div className="flex justify-end z-100 -mt-110 mx-[42px] gap-[42px]">
+                <img
+                  src={why_eat_dessert_title_desk}
+                  alt=""
+                  className="w-[88px] h-[307px] object-cover"
+                />
+                <img
+                  src={why_eat_dessert_pic}
+                  alt=""
+                  className="h-[460px] w-auto aspect-square object-cover "
+                />
+              </div>
+            </section>
+          </section>
+          <section className="block lg:hidden">
+            <section className="flex flex-col w-full">
+              <figure className="w-full h-[120px] px-[90px] flex items-center justify-center lg:w-[90px] lg:h-[308px] lg:px-0 lg:order-3 ">
+                <img
+                  src={why_make_dessert_title}
+                  alt=""
+                  className="w-full h-auto object-cover"
+                />
+              </figure>
               <img
                 src={why_make_dessert_pic}
                 alt=""
-                className="h-[460px] w-auto aspect-square object-cover "
+                className="w-full h-[250px] object-cover lg:hidden"
               />
-              <img
-                src={why_make_dessert_title_desk}
-                alt=""
-                className="w-[88px] h-[307px] object-cover"
-              />
-            </div>
-          </section>
-          <section className="hidden lg:block lg:relative ">
-            <section className="vertical-text flex flex-col w-full h-[420px] bg-soft/100 justify-end items-center">
-              <section className="max-h-[280px] text-muted overflow-hidden font-light flex flex-col whitespace-nowrap ml-[55.5px] gap-6 pr-10">
-                <p>青山依舊在，幾度夕陽紅。慣看秋月春</p>
-                <p>一壺濁酒喜相逢，浪花淘盡英雄。是非</p>
-                <p>轉頭空，滾滾長江東逝水，白髮漁樵江</p>
-                <p>古今多少事，都付笑談中。</p>
-                <p className="w-4"></p>
-                <p>是非成敗轉頭空，滾滾長江東逝水，白。</p>
-                <p>樵江渚上，古今多少事，都付笑談中。</p>
-              </section>
+              <div className="flex flex-col p-[30px] bg-soft/100 leading-8 text-muted justify-between gap-8">
+                <p>
+                  青山依舊在，幾度夕陽紅。慣看秋月春風。
+                  一壺濁酒喜相逢，浪花淘盡英雄。是非成敗
+                  轉頭空，滾滾長江東逝水，白髮漁樵江渚
+                  上，古今多少事，都付笑談中。
+                </p>
+                <p>
+                  是非成敗轉頭空，青山依舊在，幾度夕陽
+                  紅。白髮漁樵江渚上，古今多少事，都付笑 談中。
+                </p>
+              </div>
             </section>
-            <div className="flex justify-end z-100 -mt-110 mx-[42px] gap-[42px]">
-              <img
-                src={why_eat_dessert_title_desk}
-                alt=""
-                className="w-[88px] h-[307px] object-cover"
-              />
+            <section className="flex flex-col w-full">
+              <figure className="w-full h-[120px] px-[90px] flex items-center justify-center">
+                <img
+                  src={why_eat_dessert_title}
+                  alt=""
+                  className="w-full h-auto object-cover"
+                />
+              </figure>
               <img
                 src={why_eat_dessert_pic}
                 alt=""
-                className="h-[460px] w-auto aspect-square object-cover "
+                className="w-full h-[250px] object-cover"
               />
-            </div>
-          </section>
-        </section>
-        <section className="block lg:hidden">
-          <section className="flex flex-col w-full">
-            <figure className="w-full h-[120px] px-[90px] flex items-center justify-center lg:w-[90px] lg:h-[308px] lg:px-0 lg:order-3 ">
-              <img
-                src={why_make_dessert_title}
-                alt=""
-                className="w-full h-auto object-cover"
-              />
-            </figure>
-            <img
-              src={why_make_dessert_pic}
-              alt=""
-              className="w-full h-[250px] object-cover lg:hidden"
-            />
-            <div className="flex flex-col p-[30px] bg-soft/100 leading-8 text-muted justify-between gap-8">
-              <p>
-                青山依舊在，幾度夕陽紅。慣看秋月春風。
-                一壺濁酒喜相逢，浪花淘盡英雄。是非成敗
-                轉頭空，滾滾長江東逝水，白髮漁樵江渚
-                上，古今多少事，都付笑談中。
-              </p>
-              <p>
-                是非成敗轉頭空，青山依舊在，幾度夕陽
-                紅。白髮漁樵江渚上，古今多少事，都付笑 談中。
-              </p>
-            </div>
-          </section>
-          <section className="flex flex-col w-full">
-            <figure className="w-full h-[120px] px-[90px] flex items-center justify-center">
-              <img
-                src={why_eat_dessert_title}
-                alt=""
-                className="w-full h-auto object-cover"
-              />
-            </figure>
-            <img
-              src={why_eat_dessert_pic}
-              alt=""
-              className="w-full h-[250px] object-cover"
-            />
-            <div className="flex flex-col p-[30px] bg-soft/100 leading-8 text-muted justify-between gap-8">
-              <p>
-                青山依舊在，幾度夕陽紅。慣看秋月春風。
-                一壺濁酒喜相逢，浪花淘盡英雄。是非成敗
-                轉頭空，滾滾長江東逝水，白髮漁樵江渚
-                上，古今多少事，都付笑談中。
-              </p>
-              <p>
-                是非成敗轉頭空，青山依舊在，幾度夕陽
-                紅。白髮漁樵江渚上，古今多少事，都付笑 談中。
-              </p>
-            </div>
-          </section>
-        </section>
-        <section className="w-full flex flex-col">
-          <figure className="w-full h-[120px] px-[75px] flex items-center justify-center">
-            <img
-              src={want_to_eat_no_reason_title}
-              alt=""
-              className="w-full h-auto object-cover block lg:hidden"
-            />
-          </figure>
-          <section className="w-full max-w-[315px] mx-auto lg:hidden">
-            <div
-              className="overflow-hidden"
-              onTouchStart={handleTouchStart}
-              onTouchEnd={handleTouchEnd}
-            >
-              <div
-                className="flex transition-transform duration-300 ease-out"
-                style={{ transform: `translateX(-${current * 100}%)` }}
-              >
-                {productList.map((p, i) => (
-                  <div className="min-w-full  flex-shrink-0" key={i}>
-                    <ProductCard
-                      imgUrl={p.imgUrl}
-                      title={p.title}
-                      price={p.price}
-                    />
-                  </div>
-                ))}
+              <div className="flex flex-col p-[30px] bg-soft/100 leading-8 text-muted justify-between gap-8">
+                <p>
+                  青山依舊在，幾度夕陽紅。慣看秋月春風。
+                  一壺濁酒喜相逢，浪花淘盡英雄。是非成敗
+                  轉頭空，滾滾長江東逝水，白髮漁樵江渚
+                  上，古今多少事，都付笑談中。
+                </p>
+                <p>
+                  是非成敗轉頭空，青山依舊在，幾度夕陽
+                  紅。白髮漁樵江渚上，古今多少事，都付笑 談中。
+                </p>
               </div>
-            </div>
+            </section>
+          </section>
+          <section className="w-full flex flex-col items-center">
+            <figure className="w-full h-[120px] px-[75px] flex items-center justify-center lg:h-[377px] lg:mb-20">
+              <img
+                src={want_to_eat_no_reason_title}
+                alt=""
+                className="w-full h-auto object-cover block lg:hidden"
+              />
+              <img
+                src={want_to_eat_no_reason_title_desktop}
+                alt=""
+                className="hidden lg:block w-[89px] h-[377px]"
+              />
+            </figure>
+            <section className="hidden lg:flex w-full px-[42px] gap-5">
+              {productList.map((p, i) => (
+                <ProductCard
+                  key={i}
+                  imgUrl={p.imgUrl}
+                  title={p.title}
+                  price={p.price}
+                />
+              ))}
+            </section>
+            <section className="w-full max-w-[315px] mx-auto lg:hidden">
+              <div
+                className="overflow-hidden"
+                onTouchStart={handleTouchStart}
+                onTouchEnd={handleTouchEnd}
+              >
+                <div
+                  className="flex transition-transform duration-300 ease-out"
+                  style={{ transform: `translateX(-${current * 100}%)` }}
+                >
+                  {productList.map((p, i) => (
+                    <div className="min-w-full  flex-shrink-0" key={i}>
+                      <ProductCard
+                        imgUrl={p.imgUrl}
+                        title={p.title}
+                        price={p.price}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </section>
           </section>
         </section>
       </main>
