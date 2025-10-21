65892cb84ff0612f4366d57b0709b95571446790
Lin <tzu.hsun.lin046@gmail.com>
2025-10-21 10:55:33 +0800

    feat: add initial Footer component layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer =()=>{
+    return(
+    <footer className="w-full mx-auto flex flex-col">
+        <section className="w-full flex">
+
+        </section>
+        <section className="w-full">
+
+        </section>
+    </footer>
+    )
+
+}
+export default Footer
