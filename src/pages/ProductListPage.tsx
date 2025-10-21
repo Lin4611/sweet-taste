ed81329d41ac2f5885b976fbe5296711f38ffc4e
Lin <tzu.hsun.lin046@gmail.com>
2025-10-21 14:50:35 +0800

    style: finalize banner section layout and styling

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,8 +1,14 @@
 import { type FC } from "react";
+import banner_pic from "../assets/img/banner.png";
 const ProductListPage: FC = () => {
   return (
     <>
-      <main></main>
+      <main className="w-full mx-auto lg:max-w-[1024px]">
+        <figure className="w-full  h-auto lg:px-[42px] lg:h-auto">
+            <img src={banner_pic} alt="banner" className="w-full h-[277px] object-cover lg:h-auto" />
+        </figure>
+        
+      </main>
     </>
   );
 };
