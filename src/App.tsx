6f0cc7282084e60f88745d0743bfcc5fe8d35c7a
Lin <tzu.hsun.lin046@gmail.com>
2025-10-22 10:19:02 +0800

    feat: integrate Login page into App.tsx layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Navbar from "./components/Navbar";
 // import HomePage from "./pages/HomePage";
 import Footer from "./components/Footer";
-import ProductListPage from "./pages/ProductListPage";
+// import ProductListPage from "./pages/ProductListPage";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
     <>
       <Navbar />
       {/* <HomePage /> */}
-      <ProductListPage/>
+      {/* <ProductListPage/> */}
+      <LoginPage/>
       <Footer/>
     </>
   );
