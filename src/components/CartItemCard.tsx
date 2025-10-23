4792ae71ca55890d89cf841146ce04f53b72d0d7
Lin <tzu.hsun.lin046@gmail.com>
2025-10-23 12:27:54 +0800

    style: adjust Cart page product list layout for mobile and desktop

--- src/components/CartItemCard.tsx
+++ src/components/CartItemCard.tsx
@@ -1,53 +1,51 @@
-
+import delete_icon from "../assets/img/icon/delete_icon.svg";
 interface CartItemCardProps {
   imgUrl: string;
   name: string;
-  price:number;
-  count:number;
+  price: number;
+  count: number;
 }
 
-
 const CartItemCard: React.FC<CartItemCardProps> = ({
-    imgUrl,
-    name,
-    price,
-    count
+  imgUrl,
+  name,
+  price,
+  count,
 }) => {
   return (
-    <section className="w-full flex flex-col">
-      <div className="w-full flex justify-between pb-2 border-b border-soft">
-        <img src={imgUrl} alt={name} className="w-[157px] h-[106px] object-cover" />
-        <div className="flex flex-col justify-between">
-          <h4 className="text-subtitle text-primary font-light leading-5">
-            {name}
-          </h4>
-          <p className="text-primary">NT$ {price}</p>
+    <section className="w-full flex flex-col lg:flex-row">
+      <div className="w-full flex justify-between pb-2 border-b border-soft lg:justify-around lg:pb-4">
+        <img
+          src={imgUrl}
+          alt={name}
+          className="w-[157px] h-[106px] object-cover lg:w-[110px] lg:h-[110px] lg:aspect-square"
+        />
+        <div className="flex flex-col justify-between lg:flex-row lg:items-center lg:justify-around lg:w-full">
+          <div className="flex flex-col">
+            <h4 className="text-subtitle text-primary font-light leading-5">
+              {name}
+            </h4>
+            <p className="text-primary">NT$ {price}</p>
+          </div>
+
           <div className="flex text-primary text-center font-light">
-            <button
-              type="button"
-              className="py-[13px] px-[19px] border border-soft"
-            >
+            <button type="button" className="w-12 h-12 border border-soft">
               -
             </button>
-            <button
-              type="button"
-              className="py-[13px] px-[19px] border border-soft"
-            >
+            <button type="button" className="w-12 h-12 border border-soft">
               {count}
             </button>
-            <button
-              type="button"
-              className="py-[13px] px-[19px] border border-soft"
-            >
+            <button type="button" className="w-12 h-12 border border-soft">
               +
             </button>
           </div>
         </div>
       </div>
-      <section className="w-full flex justify-end items-center border-b border-soft py-2">
+      <section className="w-full flex justify-end items-center border-b border-soft py-2 lg:justify-around">
         <p className="text-subtitle text-primary font-semibold leading-5 ">
-          {price*count}
+          NT${price * count}
         </p>
+        <img src={delete_icon} alt="" className="hidden lg:block w-6 h-6 " />
       </section>
     </section>
   );
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,33 +1,53 @@
-import p1 from "../assets/img/p-1.png"
+import p1 from "../assets/img/p-1.png";
+import p2 from "../assets/img/p-2.png";
+import p3 from "../assets/img/p-3.png";
+import CartItemCard from "../components/CartItemCard";
+const CartList = [
+  {
+    id: 1,
+    imgUrl: p1,
+    name: "焦糖瑪卡龍",
+    price: 450,
+    count: 1,
+  },
+  {
+    id: 2,
+    imgUrl: p2,
+    name: "焦糖1瑪卡龍",
+    price: 450,
+    count: 3,
+  },
+  {
+    id: 3,
+    imgUrl: p3,
+    name: "焦糖2瑪卡龍",
+    price: 450,
+    count: 2,
+  },
+];
 
-const CartPage = ()=>{
-    return(
-        <>
-        <main className="w-full mx-auto lg:max-w-[1024px]">
-            <section className="w-full flex flex-col items-center">
-                <h2 className="text-title text-primary leading-5 font-semibold bg-soft/100 w-full  flex items-center justify-center py-6">您的購物車</h2>
-                <section className="w-full flex flex-col py-[30px] px-[30px] gap-[30px]">
-                    <section className="w-full flex flex-col">
-                        <div className="w-full flex justify-between pb-2 border-b border-soft">
-                            <img src={p1} alt="" className="w-[157px] h-[106px] object-cover" />
-                            <div className="flex flex-col justify-between">
-                                <h4 className="text-subtitle text-primary font-light leading-5">焦糖馬卡龍</h4>
-                                <p className="text-primary">NT$ 450</p>
-                                <div className="flex text-primary text-center font-light">
-                                    <button type="button" className="py-[13px] px-[19px] border border-soft">-</button>
-                                    <button type="button" className="py-[13px] px-[19px] border border-soft">2</button>
-                                    <button type="button" className="py-[13px] px-[19px] border border-soft">+</button>
-                                </div>
-                            </div>
-                        </div>
-                        <section className="w-full flex justify-end items-center border-b border-soft py-2">
-                            <p className="text-subtitle text-primary font-semibold leading-5 ">NT$ 900</p>
-                        </section>
-                    </section>
-                </section>
-            </section>
-        </main>
-        </>
-    )
-}
-export default CartPage
+const CartPage = () => {
+  return (
+    <>
+      <main className="w-full mx-auto lg:max-w-[1024px]">
+        <section className="w-full flex flex-col items-center">
+          <h2 className="text-title text-primary leading-5 font-semibold bg-soft/100 w-full  flex items-center justify-center py-6">
+            您的購物車
+          </h2>
+          <section className="w-full flex flex-col py-[30px] px-[30px] gap-[30px]">
+            {CartList.map((item) => (
+              <CartItemCard 
+              key={item.id}
+              name={item.name}
+              price={item.price}
+              imgUrl={item.imgUrl}
+              count={item.count}
+              />
+            ))}
+          </section>
+        </section>
+      </main>
+    </>
+  );
+};
+export default CartPage;
