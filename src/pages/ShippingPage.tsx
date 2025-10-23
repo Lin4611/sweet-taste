c8ab8feac5adf6c97829b3ea030b5f8754f67a4b
Lin <tzu.hsun.lin046@gmail.com>
2025-10-23 14:58:24 +0800

    feat: add initial shipping page and route setup in App.tsx

---
+++ src/pages/ShippingPage.tsx
@@ -0,0 +1,10 @@
+const ShippingPage =()=>{
+    return(
+        <>
+        <main className="w-full mx-auto lg:max-w-[1024px]">
+
+        </main>
+        </>
+    )
+}
+export default ShippingPage
