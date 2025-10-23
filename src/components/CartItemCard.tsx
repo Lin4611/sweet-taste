25583bbe95e013c8239c4449d40865a8cf736726
Lin <tzu.hsun.lin046@gmail.com>
2025-10-23 13:45:10 +0800

    feat: complete cart page layout and styling

--- src/components/CartItemCard.tsx
+++ src/components/CartItemCard.tsx
@@ -13,15 +13,15 @@ const CartItemCard: React.FC<CartItemCardProps> = ({
   count,
 }) => {
   return (
-    <section className="w-full flex flex-col lg:flex-row">
-      <div className="w-full flex justify-between pb-2 border-b border-soft lg:justify-around lg:pb-4">
+    <section className="w-full flex flex-col items-center lg:flex-row lg:border-b lg:border-soft">
+      <div className="w-full flex justify-between pb-2 border-b border-soft lg:justify-normal lg:pb-4 lg:gap-[39px] lg:border-0">
         <img
           src={imgUrl}
           alt={name}
           className="w-[157px] h-[106px] object-cover lg:w-[110px] lg:h-[110px] lg:aspect-square"
         />
-        <div className="flex flex-col justify-between lg:flex-row lg:items-center lg:justify-around lg:w-full">
-          <div className="flex flex-col">
+        <div className="w-full flex flex-col max-w-36 justify-between lg:flex-row lg:items-center lg:justify-between lg:max-w-[290px]">
+          <div className="flex flex-col lg:w-full">
             <h4 className="text-subtitle text-primary font-light leading-5">
               {name}
             </h4>
@@ -41,11 +41,11 @@ const CartItemCard: React.FC<CartItemCardProps> = ({
           </div>
         </div>
       </div>
-      <section className="w-full flex justify-end items-center border-b border-soft py-2 lg:justify-around">
-        <p className="text-subtitle text-primary font-semibold leading-5 ">
-          NT${price * count}
-        </p>
-        <img src={delete_icon} alt="" className="hidden lg:block w-6 h-6 " />
+      <section className="w-full flex items-center justify-between border-b border-soft py-2 lg:justify-end lg:gap-4 lg:max-w-[145px] lg:border-0 lg:p-0">
+        <label className="text-subtitle text-primary font-semibold leading-5 flex justify-center items-center">
+          <span>NT${price * count}</span>
+        </label>
+        <img src={delete_icon} alt="" className="w-6 h-6 lg:block hidden" />
       </section>
     </section>
   );
