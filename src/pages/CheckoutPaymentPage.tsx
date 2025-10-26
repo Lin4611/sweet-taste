e0fca71425adf754a30c4437e680e157de07b538
Lin <tzu.hsun.lin046@gmail.com>
2025-10-26 16:24:15 +0800

    feat(payment): add initial checkout payment page and route setup in App.tsx

---
+++ src/pages/CheckoutPaymentPage.tsx
@@ -0,0 +1,16 @@
+const CheckoutPaymentPage = ()=>{
+    return(
+        <>
+        <main className="w-full mx-auto lg:max-w-[1024px]">
+            <section className="w-full flex flex-col">
+                <div className="w-full flex flex-col gap-[29px] px-[30px]">
+                    <div className="flex justify-between items-center w-full">
+                        <h2>付款</h2>
+                    </div>
+                </div>
+            </section>
+        </main>
+        </>
+    )
+}
+export default CheckoutPaymentPage
