f2cab7cf3682918f70e57f0386fc3dcf99bfda69
Lin <tzu.hsun.lin046@gmail.com>
2025-10-27 10:23:32 +0800

    feat(navbar): add page links and integrate with router navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/img/logo/logo-all-dark.svg";
 import cart from "../assets/img/icon/shopping_cart.png";
 import logo_phone from "../assets/img/logo/logotype-sm-dark.svg";
 import menu_icon from "../assets/img/icon/dehaze-24px.png";
-
+import { Link } from "react-router-dom";
 const Navbar: FC = () => {
   const [navbarShow, setNavbarShow] = useState<boolean>(false);
   const handleClick: React.MouseEventHandler<HTMLButtonElement> = () => {
@@ -12,38 +12,93 @@ const Navbar: FC = () => {
   return (
     <header className=" w-full mx-auto sticky top-0 z-[100] bg-white py-[36px] lg:py-[30px] lg:sticky lg:max-w-[1024px]">
       <nav className="mx-auto flex items-center justify-between w-full px-[33px] lg:max-w-[940px]">
-        <button type="button" onClick={handleClick} className="blcok relative active:bg-accent/30 rounded-2xl lg:hidden">
-          <img src={menu_icon} alt="menu"/>
+        <button
+          type="button"
+          onClick={handleClick}
+          className="blcok relative active:bg-accent/30 rounded-2xl lg:hidden"
+        >
+          <img src={menu_icon} alt="menu" />
         </button>
-        <img
-          src={logo}
-          alt="logo"
-          className="hidden lg:block h-[40px] w-auto object-cover"
-        />
-        <img
-          src={logo_phone}
-          alt="logo"
-          className="block h-[18px] w-auto object-cover lg:hidden"
-        />
+        <Link to="/">
+          <img
+            src={logo}
+            alt="logo"
+            className="hidden lg:block h-[40px] w-auto object-cover"
+          />
+        </Link>
+        <Link to="/">
+          <img
+            src={logo_phone}
+            alt="logo"
+            className="block h-[18px] w-auto object-cover lg:hidden"
+          />
+        </Link>
+
         <div className="items-center justify-between lg:flex lg:max-w-[34%] lg:w-full">
           <div className="hidden lg:max-w-[70%] lg:flex w-full items-center justify-between">
-            <a href="#" className="text-primary font-semibold hover:text-xl hover:shadow-2xl hover:shadow-primary">首頁</a>
-            <a href="#" className="text-primary font-semibold hover:text-xl hover:shadow-2xl hover:shadow-primary">甜點</a>
-            <a href="#" className="text-primary font-semibold hover:text-xl hover:shadow-2xl hover:shadow-primary">登入</a>
+            <Link
+              to="/"
+              className="text-primary font-semibold hover:text-xl hover:shadow-2xl hover:shadow-primary"
+            >
+              首頁
+            </Link>
+
+            <Link
+              to="/products"
+              className="text-primary font-semibold hover:text-xl hover:shadow-2xl hover:shadow-primary"
+            >
+              甜點
+            </Link>
+            <Link
+              to="/login"
+              className="text-primary font-semibold hover:text-xl hover:shadow-2xl hover:shadow-primary"
+            >
+              登入
+            </Link>
           </div>
-          <img
-            src={cart}
-            alt=""
-            className="h-[24px] w-auto object-cover aspect-square hover:h-[30px] hover:shadow-lg hover:shadow-accent hover:rounded-2xl active:h-[30px]"
-          />
+          <Link to="/cart">
+            <img
+              src={cart}
+              alt=""
+              className="h-[24px] w-auto object-cover aspect-square hover:h-[30px] hover:shadow-lg hover:shadow-accent hover:rounded-2xl active:h-[30px]"
+            />
+          </Link>
         </div>
       </nav>
-      <div className={`lg:hidden absolute left-0 right-0 bg-white shadow-lg transition-all duration-300 ease-in-out overflow-hidden ${navbarShow ? 'max-h-[300px] opacity-100' : 'max-h-0 opacity-0'}`}>
-          <div className="flex flex-col w-full items-center pt-[36px]">
-            <a href="#" className="text-primary font-semibold hover:text-xl active:bg-accent/30 active:rounded-2xl" onClick={()=>{setNavbarShow(false)}}>首頁</a>
-            <a href="#" className="text-primary font-semibold hover:text-xl active:bg-accent/30 active:rounded-2xl" onClick={()=>{setNavbarShow(false)}}>甜點</a>
-            <a href="#" className="text-primary font-semibold hover:text-xl active:bg-accent/30 active:rounded-2xl" onClick={()=>{setNavbarShow(false)}}>登入</a>
-          </div>
+      <div
+        className={`lg:hidden absolute left-0 right-0 bg-white shadow-lg transition-all duration-300 ease-in-out overflow-hidden ${
+          navbarShow ? "max-h-[300px] opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
+        <div className="flex flex-col w-full items-center pt-[36px]">
+          <Link
+            to="/"
+            className="text-primary font-semibold hover:text-xl active:bg-accent/30 active:rounded-2xl"
+            onClick={() => {
+              setNavbarShow(false);
+            }}
+          >
+            首頁
+          </Link>
+          <Link
+            to="/products"
+            className="text-primary font-semibold hover:text-xl active:bg-accent/30 active:rounded-2xl"
+            onClick={() => {
+              setNavbarShow(false);
+            }}
+          >
+            甜點
+          </Link>
+          <Link
+            to="/login"
+            className="text-primary font-semibold hover:text-xl active:bg-accent/30 active:rounded-2xl"
+            onClick={() => {
+              setNavbarShow(false);
+            }}
+          >
+            登入
+          </Link>
+        </div>
       </div>
     </header>
   );
